9bfea338f3e71047a64a86487e9bfa46711938f8
tomato-data <ahsdfg30@gmail.com>
2026-02-18 16:38:44 +0900

    BUGFIX: Fix bug where total assets was not calculated as expected

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,8 +9,8 @@ class DashboardController < ApplicationController
     @transactions_by_date = @transactions.group_by(&:date)
     @total_income = @transactions.where(transaction_type: :income).sum(:amount)
     @total_expense = @transactions.where(transaction_type: :expense).sum(:amount)
-    @total_assets = current_user.transactions.where(transaction_type: :income).sum(:amount)
-                      - current_user.transactions.where(transaction_type: :expense).sum(:amount)
+    @total_assets = current_user.transactions.where(transaction_type: :income).sum(:amount) -
+                      current_user.transactions.where(transaction_type: :expense).sum(:amount)
   end
 
   def daily_transactions
