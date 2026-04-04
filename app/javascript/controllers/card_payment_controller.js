9ed7d4be0a8d5ad2959747504dab1b0fb0f94142
tomato-data <ahsdfg30@gmail.com>
2026-04-04 14:08:57 +0900

    Feat: Add status section disabling logic when the transcation type is card

--- app/javascript/controllers/card_payment_controller.js
+++ app/javascript/controllers/card_payment_controller.js
@@ -4,10 +4,23 @@ export default class extends Controller {
   static targets = ["toggle", "details"]
 
   toggle() {
-    if (this.toggleTarget.checked) {
+    const isCard = this.toggleTarget.checked
+    const statusSection = document.querySelector(
+      '[data-transaction-form-target="statusSection"]'
+    )
+
+    if (isCard) {
       this.detailsTarget.classList.remove("hidden")
+      if (statusSection) {
+        statusSection.style.opacity = "0.4"
+        statusSection.style.pointerEvents = "none"
+      }
     } else {
       this.detailsTarget.classList.add("hidden")
+      if (statusSection) {
+        statusSection.style.opacity = ""
+        statusSection.style.pointerEvents = ""
+      }
     }
   }
 }
